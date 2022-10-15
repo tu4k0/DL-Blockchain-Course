adea23c31537db5934df4244a1eab9f387d7c83f
tu4k0 <tu4k0>
2022-10-15 12:30:33 +0300

    Update Hex_Int_Lib.py and Main.py

--- Practice#2/Hex_Int_Lib.py
+++ Practice#2/Hex_Int_Lib.py
@@ -1,2 +1,11 @@
-def from_hex_to_lil_endian():
-    print('Practice#2 start')
+def from_hex_to_lil_endian(value):
+    print(f'lil endian: {value}')
+
+def from_hex_to_big_endian(value):
+    print(f'big endian: {value}')
+
+def from_lil_endian_to_hex(value):
+    print(f'hex: {value}')
+
+def from_big_endian_to_hex(value):
+    print(f'hex: {value}')
--- Practice#2/Main.py
+++ Practice#2/Main.py
@@ -1,7 +1,9 @@
 import Hex_Int_Lib as hil
 
+
 def main():
-    hil.from_hex_to_lil_endian()
+    hex_value = input("Введите hex значение которое хотите преобразовать: ")
+    hil.from_hex_to_lil_endian(hex(int(hex_value, 16)))
 
 
 if __name__ == '__main__':
