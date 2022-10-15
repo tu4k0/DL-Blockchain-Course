1520e2c00e82c3ebb7517b41eb74dac29bd7f5f2
tu4k0 <tu4k0>
2022-10-15 16:20:00 +0300

    Update Hex_Int_Lib.py (Create function from_hex_to_little_indian()). Update Main.py (Modified main() function).

--- Practice#2/Hex_Int_Lib.py
+++ Practice#2/Hex_Int_Lib.py
@@ -1,11 +1,25 @@
-def from_hex_to_lil_endian(value):
-    print(f'lil endian: {value}')
+def from_hex_to_little_endian(value):
+    hex_bytes = value.to_bytes(value.bit_length() // 8, byteorder='little')
+    hex_bytes_list = list(hex_bytes)
+    hex_bytes_amount = value.bit_length() // 8
+    little_endian_bytes_list = ''
+    little_endian_hex = '0x'
+    for byte in range(0, len(hex_bytes_list)):
+        little_endian_bytes_list += str(hex(hex_bytes_list[byte]))
+    little_endian_bytes_list = little_endian_bytes_list[:].split('0x')
+    for byte in range(0, len(little_endian_bytes_list)):
+        little_endian_hex += str(little_endian_bytes_list[byte])
+    little_endian = int(little_endian_hex, 16)
+    return little_endian, hex_bytes_amount
+
 
 def from_hex_to_big_endian(value):
     print(f'big endian: {value}')
 
-def from_lil_endian_to_hex(value):
+
+def from_little_endian_to_hex(value):
     print(f'hex: {value}')
+
 
 def from_big_endian_to_hex(value):
     print(f'hex: {value}')
--- Practice#2/Main.py
+++ Practice#2/Main.py
@@ -2,8 +2,9 @@ import Hex_Int_Lib as hil
 
 
 def main():
-    hex_value = input("Введите hex значение которое хотите преобразовать: ")
-    hil.from_hex_to_lil_endian(hex(int(hex_value, 16)))
+    hex_value = int(input("Введите HEX значение которое хотите преобразовать в Little-/Big- Endian: "), 16)
+    little_endian, bytes = hil.from_hex_to_little_endian(hex_value)
+    print(little_endian, bytes)
 
 
 if __name__ == '__main__':
