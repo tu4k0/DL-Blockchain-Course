31af0dd13f6833219b7e28159325949b1838041e
tu4k0 <tu4k0>
2022-10-16 00:39:11 +0300

    Update Main.py

--- Practice#2/Main.py
+++ Practice#2/Main.py
@@ -4,7 +4,9 @@ import Hex_Int_Lib as hil
 def main():
     hex_value = int(input("Введите HEX значение которое хотите преобразовать в Little-/Big- Endian: "), 16)
     little_endian, bytes = hil.from_hex_to_little_endian(hex_value)
+    big_endian, bytes = hil.from_hex_to_big_endian(hex_value)
     print(little_endian, bytes)
+    print(big_endian)
 
 
 if __name__ == '__main__':
