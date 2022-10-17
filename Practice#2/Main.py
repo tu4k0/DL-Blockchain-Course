bf0f7cf6a0169b06e62557df9dafa30f80f08891
tu4k0 <tu4k0>
2022-10-17 16:26:00 +0300

    Update Main.py (Final version of solution).

--- Practice#2/Main.py
+++ Practice#2/Main.py
@@ -2,15 +2,28 @@ import Hex_Int_Lib as hil
 
 
 def main():
-    # hex_value = int(input("Введите HEX значение которое хотите преобразовать в Little-/Big- Endian: "), 16)
-    # little_endian, bytes = hil.from_hex_to_little_endian(hex_value)
-    # big_endian, bytes = hil.from_hex_to_big_endian(hex_value)
-    # print(little_endian, bytes)
-    int_value = int(input("Введите HEX значение которое хотите преобразовать в Little-/Big- Endian: "))
-    # hex = hil.from_little_endian_to_hex(int_value,32)
-    big_hex = hil.from_big_endian_to_hex(int_value,32)
-    print(big_hex)
-    # print(big_endian)
+    print('\n')
+    print("-" * 100)
+    print("a. Convert HEX --> Little-Endian")
+    hex_value = input("Enter HEX value: ")
+    little_endian, little_endian_bytes = hil.from_hex_to_little_endian(hex_value)
+    print(f"Number of bytes: {little_endian_bytes}\nLittle-endian: {little_endian}")
+    print("-"*100)
+    print("b. Convert HEX --> Big-Endian")
+    hex_value = input("Enter HEX value: ")
+    big_endian, big_endian_bytes = hil.from_hex_to_big_endian(hex_value)
+    print(f"Number of bytes: {big_endian_bytes}\nBig-endian: {big_endian}")
+    print("-" * 100)
+    print("c. Convert Little-Endian --> HEX")
+    little_endian = input("Enter Little-Endian value: ")
+    hex_value = hil.from_little_endian_to_hex(little_endian, little_endian_bytes)
+    print(f"Number of bytes: {little_endian_bytes}\nHEX value: {hex_value}")
+    print("-" * 100)
+    print("d. Convert Big-Endian --> HEX")
+    big_endian = input("Enter Big-Endian value: ")
+    hex_value = hil.from_big_endian_to_hex(big_endian, big_endian_bytes)
+    print(f"Number of bytes: {big_endian_bytes}\nHEX value: {hex_value}")
+    print("-" * 100)
 
 
 if __name__ == '__main__':
